a6e9c946a27efa9d4067e99a832cf64347b8c516
Игорь Шулая <i.shulaya@gmail.com>
2022-05-29 22:08:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string MyName;
+        MyName = " Mark ";
+        Console.WriteLine(MyName);
+        Console.ReadKey();
+    }
+}
